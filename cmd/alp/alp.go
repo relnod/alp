114a04a17200a1c6692ac4f809f951645dd18188
relnod <mail@paul-schiffers.de>
2018-02-12 00:11:06 +0100

    source: implement eval source

--- cmd/alp/alp.go
+++ cmd/alp/alp.go
@@ -8,18 +8,44 @@ import (
 )
 
 func main() {
-	s := source.NewFileSource("bla.alp")
-
-	t := token.Token{
+	fs := source.NewFileSource("test/bla.alp")
+	t1 := token.Token{
 		Type:  token.Int,
 		Value: "123",
 		Span: token.Span{
-			Source: &s,
+			Source: &fs,
+			Line:   10,
+			ColBeg: 5,
+			ColEnd: 10,
+		},
+	}
+	r, err := fs.GetReader()
+	if err != nil {
+		panic(err)
+	}
+
+	buffer := make([]byte, 10)
+	r.Read(buffer)
+	fmt.Println(string(buffer))
+
+	fmt.Println(t1)
+
+	es := source.NewEvalSource("123", "let a: int = 2")
+	t2 := token.Token{
+		Type:  token.Int,
+		Value: "123",
+		Span: token.Span{
+			Source: &es,
 			Line:   10,
 			ColBeg: 5,
 			ColEnd: 10,
 		},
 	}
 
-	fmt.Println(t)
+	fmt.Println(t2)
+
+	r2, _ := fs.GetReader()
+
+	r2.Read(buffer)
+	fmt.Println(string(buffer))
 }
