db9cb775207dd86fb11789b9d27db8e9c38d0ea4
relnod <mail@paul-schiffers.de>
2018-02-11 21:23:42 +0100

    test: use ginkgo for testing

--- token/token_test.go
+++ token/token_test.go
@@ -3,6 +3,8 @@ package token_test
 import (
 	"testing"
 
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 	"github.com/relnod/alp/token"
 )
 
@@ -22,3 +24,27 @@ func TestTokenTypeToString(t *testing.T) {
 		t.Error()
 	}
 }
+
+func TestToken(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Token Suite")
+}
+
+var _ = Describe("token.Type", func() {
+	Describe("String()", func() {
+		It("converts to a string if type is valid", func() {
+			t := token.Int
+			Expect(t.String()).To(Equal("Int"))
+		})
+
+		It("doesn't convert correctly for internal type", func() {
+			t := token.Type(3)
+			Expect(t.String()).To(Equal("Unkown Token: 3"))
+		})
+
+		It("doesn't convert correctly for undefined type", func() {
+			t := token.Type(255)
+			Expect(t.String()).To(Equal("Unkown Token: 255"))
+		})
+	})
+})
